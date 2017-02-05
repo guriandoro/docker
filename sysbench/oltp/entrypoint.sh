198b92df5eaeaec746faef2cc69f6f02622e5ec3
ぐりあんどろ <guriandoro@gmail.com>
2017-02-05 18:40:33 -0300

    Fixed typo on table count variable.

--- sysbench/oltp/entrypoint.sh
+++ sysbench/oltp/entrypoint.sh
@@ -11,7 +11,7 @@ MYSQL_DB="${MYSQL_DB:-test}"
 
 OLTP_TEST=/usr/share/doc/sysbench/tests/db/oltp.lua
 OLTP_TABLE_SIZE="${OLTP_TABLE_SIZE:-250000}"
-OLTP_TABLE_COUNT="${OLTP_TABLE_COUNT:-1}"
+OLTP_TABLES_COUNT="${OLTP_TABLES_COUNT:-1}"
 
 REPORT_INTERVAL="${REPORT_INTERVAL:-1}"
 MAX_REQUESTS="${MAX_REQUESTS:-0}"
@@ -22,7 +22,7 @@ echo ======= Using the following variables =======
 
 echo OLTP_TEST $OLTP_TEST
 echo OLTP_TABLE_SIZE $OLTP_TABLE_SIZE
-echo OLTP_TABLE_COUNT $OLTP_TABLE_COUNT
+echo OLTP_TABLES_COUNT $OLTP_TABLES_COUNT
 echo MYSQL_HOST $MYSQL_HOST
 echo MYSQL_USER $MYSQL_USER
 echo MYSQL_PASS $MYSQL_PASS
@@ -40,7 +40,7 @@ sysbench --test=$OLTP_TEST \
 --mysql-password=$MYSQL_PASS \
 --mysql-db=$MYSQL_DB \
 --oltp-table-size=$OLTP_TABLE_SIZE \
---oltp-table-count=$OLTP_TABLE_COUNT \
+--oltp-tables-count=$OLTP_TABLES_COUNT \
 prepare
 
 echo
@@ -52,9 +52,8 @@ sysbench --test=$OLTP_TEST \
 --mysql-password=$MYSQL_PASS \
 --mysql-db=$MYSQL_DB \
 --oltp-table-size=$OLTP_TABLE_SIZE  \
---oltp-table-count=$OLTP_TABLE_COUNT \
+--oltp-tables-count=$OLTP_TABLES_COUNT \
 --report-interval=$REPORT_INTERVAL \
 --max-requests=$MAX_REQUESTS \
 --tx-rate=$TX_RATE \
 run | grep tps
-
